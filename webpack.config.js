dcb251c9e615e7a35c464ced8cff3f97ea443b88
Benjamin Saphier <b.saphier@gmail.com>
2016-12-13 11:29:42 -0500

    default commit message

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+'use strict';
+
+ const webpack = require('webpack');
+
+module.exports = {
+  entry: './browser/react/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  context: __dirname,
+  module: {
+    loaders: [
+      {
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
