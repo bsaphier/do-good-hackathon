2736fbe8a5db1633b5bab52c446c279109a7dfea
Charity Corcino <cecicorcino@gmail.com>
2016-12-13 12:49:22 -0500

    #4

--- server/app/seed.js
+++ server/app/seed.js
@@ -3,36 +3,36 @@ var posts = [
         location: "Times Square (42nd & 7th)",
         date: Date.UTC(2017, 1, 8, 21, 30),
         title: "Horrible Hair Salon",
-        scam: "A man came up to me and told me for $15 I would be able to get my hair cut and styled at a nearby 'Upscale' salon. Well, the joke was on me because I got what I paid for and I am NOT happy! I can do a better job cutting my own hair. Buyer beware.",
-        description: "NY"
+        scam: "A woman came up to me and told me for $15 I would be able to get my hair cut and styled at a nearby 'Upscale' salon. Well, the joke was on me because I got what I paid for and I am NOT happy! I can do a better job cutting my own hair. Buyer beware.",
+        description: "Tall, skinny, woman "
     },
     {
         location: "Penn Station (34th street)",
         date: Date.UTC(2017, 1, 8, 21, 30),
-        title: "52 Mulberry St",
-        scam: "New York",
+        title: "Nickelodeon Dude",
+        scam: "Some guy came up to me and asked for directions to East Village. Then he started to compliment me on my voice and look. He said he was a Nickelodeon executive and he could get me on upcoming season of some show Selena Gomez is on if I give him $3,000 for the audition. A couple of meetings later and calls to a now disconnected number proves that he is a scam artist. Now, I am out of $3,000! :(",
+        description: ""
+
+    },
+    {
+        location: "Canal Street",
+        date: Date.UTC(2017, 1, 8, 21, 30),
+        title: "Fake Handbags",
+        scam: "I'm from North Dakota and this is my first time visiting New York. I was so excited to see the Empire State building and Statue of Liberty (typical tourist things, don't judge me). So I after my sister and I got off the train at Canal street we saw some designer handbags. We were so happy to get expensive bags at such a discount! About a month later after parading around town with our New York fashionista bags, they started to fall apart at the seams...Literally! We were conned out of a couple of hundred dollars ;-( RIP to my knock-off Louis Vuitton ",
         description: "NY"
 
     },
     {
-        location: "The Ritz-Carlton New York, Battery Park",
-        date: Date.UTC(2017, 1, 8, 21, 30)
-        title: "Two West Street",
-        scam: "New York",
-        description: "NY"
-
-    },
-    {
-        location: "Wall Street Inn",
-        date: Date.UTC(2017, 1, 8, 21, 30)
-        title: "9 S William St",
-        scam: "New York",
+        location: "Williamsburg, Brooklyn",
+        date: Date.UTC(2017, 1, 8, 21, 30),
+        title: "Where's the Jazz?",
+        scam: "My girlfriend and I were enjoying the totally hip vibe of Williamsburg and a man on the street told us about this cool underground jazz showcase a couple of blocks away. The great thing about it was that he happen to have tickets to get in because it was so exclusive so we paid him $40 and he gave us directions to the den. We GPS the address just to make sure and we arrived at deli store (I think the locals call it a bodega). The man at the counter laughed at us when we showed him the tickets. ",
         description: "NY"
 
     },
     {
         location: "Smyth TriBeCa",
-        date: Date.UTC(2017, 1, 8, 21, 30)
+        date: Date.UTC(2017, 1, 8, 21, 30),
         title: "85 West Broadway",
         scam: "New York",
         description: "NY"
@@ -40,7 +40,7 @@ var posts = [
     },
     {
         location: "Double Tree",
-        date: Date.UTC(2017, 1, 8, 21, 30)
+        date: Date.UTC(2017, 1, 8, 21, 30),
         title: "8 Stone St",
         scam: "New York",
         description: "NY"
@@ -48,7 +48,7 @@ var posts = [
     },
     {
         location: "Hotel 91",
-        date: Date.UTC(2017, 1, 8, 21, 30)
+        date: Date.UTC(2017, 1, 8, 21, 30),
         title: "91 E Broadway",
         scam: "New York",
         description: "NY"
