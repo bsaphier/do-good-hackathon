6a1b10eac490d0f9da344bdb41889e1f48ef7837
Charity Corcino <cecicorcino@gmail.com>
2016-12-13 12:18:21 -0500

    Example 2

--- server/app/seed.js
+++ server/app/seed.js
@@ -1,79 +1,81 @@
-var data = {
-    posts: [
-        {location: "Times Square (42nd & 7th)",
-            date: "10/25/2016",
-                title: "Horrible Hair Salon",
-                scam: "Salon Voucher",
-                description: "A man came up to me and told me for $15 I would be able to get my hair cut and styled at a nearby 'Upscale' salon. Well, the joke was on me because I got what I paid for and I am NOT happy! I can do a better job cutting my own hair. Buyer beware."
-            
-        },
-        {location: "Penn Station (34th street)",
-            date: {
-                title: "52 Mulberry St",
-                scam: "New York",
-                description: "NY"
-            }
-        },
-        {location: "The Ritz-Carlton New York, Battery Park",
-            date: {
-                title: "Two West Street",
-                scam: "New York",
-                description: "NY"
-            }
-        },
-        {location: "Wall Street Inn",
-            date: {
-                title: "9 S William St",
-                scam: "New York",
-                description: "NY"
-            }
-        },
-        {location: "Smyth TriBeCa",
-            date: {
-                title: "85 West Broadway",
-                scam: "New York",
-                description: "NY"
-            }
-        },
-        {location: "Double Tree",
-            date: {
-                title: "8 Stone St",
-                scam: "New York",
-                description: "NY"
-            }
-        },
-        {location: "Hotel 91",
-            date: {
-                title: "91 E Broadway",
-                scam: "New York",
-                description: "NY"
-            },
-        },
-        {location: "Conrad New York Hotel",
-            date: {
-                title: "102 N End Ave",
-                scam: "New York",
-                description: "NY"
-            }
-        },
-        {location: "Millenium Hilton Hotel",
-            date: {
-                title: "55 Church St",
-                scam: "New York",
-                description: "NY"
-            }
-        },
-        {location: "US Pacific Hotel",
-            description: {
-                title: "106 Bowery",
-                scam: "New York",
-                description: "NY"
-            }
-        },
-        {title: "Gild Hall, a Thompson Hotel",
-            description: {
-                title: "15 Gold Street",
-                scam: "New York",
-                description: "NY"
-            }
-        }
+var posts = [
+    {
+        location: "Times Square (42nd & 7th)",
+        date: Date.UTC(2017, 1, 8, 21, 30),
+        title: "Horrible Hair Salon",
+        scam: "A man came up to me and told me for $15 I would be able to get my hair cut and styled at a nearby 'Upscale' salon. Well, the joke was on me because I got what I paid for and I am NOT happy! I can do a better job cutting my own hair. Buyer beware.",
+        description: "NY"
+    },
+    {
+        location: "Penn Station (34th street)",
+        date: Date.UTC(2017, 1, 8, 21, 30),
+        title: "52 Mulberry St",
+        scam: "New York",
+        description: "NY"
+
+    },
+    {
+        location: "The Ritz-Carlton New York, Battery Park",
+        date: Date.UTC(2017, 1, 8, 21, 30)
+        title: "Two West Street",
+        scam: "New York",
+        description: "NY"
+
+    },
+    {
+        location: "Wall Street Inn",
+        date: Date.UTC(2017, 1, 8, 21, 30)
+        title: "9 S William St",
+        scam: "New York",
+        description: "NY"
+
+    },
+    {
+        location: "Smyth TriBeCa",
+        date: Date.UTC(2017, 1, 8, 21, 30)
+        title: "85 West Broadway",
+        scam: "New York",
+        description: "NY"
+
+    },
+    {
+        location: "Double Tree",
+        date: Date.UTC(2017, 1, 8, 21, 30)
+        title: "8 Stone St",
+        scam: "New York",
+        description: "NY"
+
+    },
+    {
+        location: "Hotel 91",
+        date: Date.UTC(2017, 1, 8, 21, 30)
+        title: "91 E Broadway",
+        scam: "New York",
+        description: "NY"
+
+    },
+    {
+        location: "Conrad New York Hotel",
+        date: Date.UTC(2017, 1, 8, 21, 30)
+        title: "102 N End Ave",
+        scam: "New York",
+        description: "NY"
+
+    },
+    {
+        location: "Millenium Hilton Hotel",
+        date: Date.UTC(2017, 1, 8, 21, 30)
+        title: "55 Church St",
+        scam: "New York",
+        description: "NY"
+
+    },
+    {
+        location: "US Pacific Hotel",
+        date: Date.UTC(2017, 1, 8, 21, 30)
+        title: "106 Bowery",
+        scam: "New York",
+        description: "NY"
+
+    }
+]
