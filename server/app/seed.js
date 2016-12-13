94d92eac1809498051630813a62a6219481886b8
Charity Corcino <cecicorcino@gmail.com>
2016-12-13 12:05:02 -0500

    Example 1

---
+++ server/app/seed.js
@@ -0,0 +1,79 @@
+var data = {
+    posts: [
+        {location: "Times Square (42nd & 7th)",
+            date: "10/25/2016",
+                title: "Horrible Hair Salon",
+                scam: "Salon Voucher",
+                description: "A man came up to me and told me for $15 I would be able to get my hair cut and styled at a nearby 'Upscale' salon. Well, the joke was on me because I got what I paid for and I am NOT happy! I can do a better job cutting my own hair. Buyer beware."
+            
+        },
+        {location: "Penn Station (34th street)",
+            date: {
+                title: "52 Mulberry St",
+                scam: "New York",
+                description: "NY"
+            }
+        },
+        {location: "The Ritz-Carlton New York, Battery Park",
+            date: {
+                title: "Two West Street",
+                scam: "New York",
+                description: "NY"
+            }
+        },
+        {location: "Wall Street Inn",
+            date: {
+                title: "9 S William St",
+                scam: "New York",
+                description: "NY"
+            }
+        },
+        {location: "Smyth TriBeCa",
+            date: {
+                title: "85 West Broadway",
+                scam: "New York",
+                description: "NY"
+            }
+        },
+        {location: "Double Tree",
+            date: {
+                title: "8 Stone St",
+                scam: "New York",
+                description: "NY"
+            }
+        },
+        {location: "Hotel 91",
+            date: {
+                title: "91 E Broadway",
+                scam: "New York",
+                description: "NY"
+            },
+        },
+        {location: "Conrad New York Hotel",
+            date: {
+                title: "102 N End Ave",
+                scam: "New York",
+                description: "NY"
+            }
+        },
+        {location: "Millenium Hilton Hotel",
+            date: {
+                title: "55 Church St",
+                scam: "New York",
+                description: "NY"
+            }
+        },
+        {location: "US Pacific Hotel",
+            description: {
+                title: "106 Bowery",
+                scam: "New York",
+                description: "NY"
+            }
+        },
+        {title: "Gild Hall, a Thompson Hotel",
+            description: {
+                title: "15 Gold Street",
+                scam: "New York",
+                description: "NY"
+            }
+        }
